b4d615f999b7e8c2ff9a18df2b7e35cc7104b0d8
artsiom lamaka <artem.lomako@iridescent.studio>
2025-02-10 22:51:44 +0100

    fix issues with dependencies (whatwg-urls) and during the starting of app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
     }),
     UsersModule,
   ],
